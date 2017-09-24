211af069c6f7330c17471196a61ea3077f97c3d6
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-24 09:10:14 -0700

    Set up collection view with IBOutlets

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Katherine Reinhart on 9/24/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
+}
